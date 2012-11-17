5c1366a63db24abf7659722e080cd829baf914b3
Steven David <stevndavid@gmail.com>
2012-11-17 11:31:16 +0700

    last updated by stevndavid - 17 Nov 2012

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Home | Griya Hijau</title>
+        <link rel="stylesheet" type="text/css" href="source/style/css/style.css" media="screen" title="style (screen)" />
+        <link rel="stylesheet" type="text/css" href="source/style/css/menu.css" media="screen" title="style (screen)" />
+        
+        
+		<link type="text/css" href="style/jquery-ui-1.9.1.custom.min.css" rel="stylesheet" />
+        <link type="text/css" href="style/menu.css" rel="stylesheet" />
+        <link type="text/css" href="style/style.css" rel="stylesheet" />
+		<script type="text/javascript" src="js/jquery-1.8.2.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.9.1.custom.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$(".slidingDiv").hide();
+				$(".show_hide").show();
+				
+				$('.show_hide').click(function(){
+					$(".slidingDiv").slideToggle("slow");
+				});
+
+                $(function() {
+                    $("#tab-project-1").tabs();
+                    $("#accordion-projects").accordion({
+                        heightStyle: "content",
+                        collapsible: "true"
+                    });
+                });
+			});
+		</script>
+        
+        <!--Map-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta charset="UTF-8">
+        
+        <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+            <script type="text/javascript">
+                var map;
+                var mainMap;
+                function initialize() {
+                    var myOptions = {
+                        panControl: false,
+                        zoomControl: false,
+                        mapTypeControl: false,
+                        scaleControl: false,
+                        streetViewControl: false,
+                        overviewMapControl: false,
+                        zoom: 15,
+                        center: new google.maps.LatLng(-6.23779, 106.62894),
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map(document.getElementById('contactUS_mainFrame_right_Map'),myOptions);
+              }
+        
+            google.maps.event.addDomListener(window, 'load', initialize);
+        </script>
+        <!--End Map-->
+
+
+        <!-- FONTS -->
+        <link href='http://fonts.googleapis.com/css?family=Petit+Formal+Script|Patua+One' rel='stylesheet' type='text/css'>
+	</head>
+    
+    <body>
+        <div id="mainHeader"></div>
+        <div id="toolbar" class="dropShadow">
+            <div id="toolbar-header">
+                GRIYA HIJAU Development
+            </div>
+            <ul id="toolbar-menu">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#about-us">About Us</a></li>
+                <li><a href="javascript:void(0)" class="show_hide">Contact Us</a></li>
+                <li>|</li>
+                <li>Stay Connected</li>
+                <li class="icon-facebook"><a href=""></a></li>
+                <li class="icon-twitter"><a href=""></a></li>
+            </ul>
+        </div>
+        <div id="sections">
+            <div id="home">
+                <div class="section-header">Griya Hijau Development</div>
+            	<div id="content">
+                	<p>
+                    	Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+                    </p>
+                    
+                    <p>
+                    	Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
+                    </p>
+                    
+                    <p>
+                    </p>
+                </div>
+            </div>
+
+            <div class="divider"></div>
+
+            <div id="projects">
+                <div class="section-header">Projects</div>
+                <div id="accordion-projects">
+                    <h3>Project 1</h3>
+                    <div>
+
+                        <!-- tabs -->
+                        <div id="tab-project-1">
+                            <ul>
+                                <li><a href="#tabs-1">Subproject 1</a></li>
+                                <li><a href="#tabs-2">Subproject 2</a></li>
+                                <li><a href="#tabs-3">Subproject 3</a></li>
+                            </ul>
+                            <div id="tabs-1">
+                                <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+                            </div>
+                            <div id="tabs-2">
+                                <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+                            </div>
+                            <div id="tabs-3">
+                                <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+                                <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
+                            </div>
+                        </div>
+                        <!-- tabs -->
+
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+                    </div>
+
+                    <h3>Project 2</h3>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+                    </div>
+
+                    <h3>Project 3</h3>
+                    <div>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+                    </div>
+                </div>
+            </div>
+
+            <div class="divider"></div>
+
+            <div id="about-us">
+                <div class="section-header">About Us</div>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+            </div>
+        </div>
+        
+        <div class="slidingDiv">
+        	<div id="slidingHeader" class="dropShadow_lighter"></div>
+            <div id="slidingContent">
+            	<div id="contactUS_mainFrame">
+                	<div id="contactUS_mainFrame_left">
+                	  <form name="frm_sendEmail" action="sendEmail.php" method="post">
+               	  <table width="100%">
+                        	<tr>
+                                <td>
+                                	Name <span class="required">(required*)</span><br />
+                                    <input type="text" name="tb_name" id="tb_name" class="inputTextStyle" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Email Address <span class="required">(required*)</span><br />
+                                    <input type="text" name="tb_email" id="tb_email" class="inputTextStyle" />
+                                </td>
+                            </tr>
+                            <tr>
+                            	<td>
+                                	Your Message <span class="required">(required*)</span><br />
+                                    <textarea name="tb_email" id="tb_email" class="inputTextAreaStyle" ></textarea>
+                                </td>
+                            </tr>
+                            <tr>
+                            	<td>
+                                	<input type="submit" value="Send Message" class="fsSubmitButton" />
+                                </td>
+                            </tr>
+                        </table>
+                        </form>
+                    </div>
+            	</div>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            $(function() {
+                $('#toolbar-menu li a').bind('click', function(event){
+                    var $anchor = $(this);
+                    
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1500,'easeInOutExpo');
+
+                    event.preventDefault();
+                });
+            });
+        </script>
+    </body>
+</html>
